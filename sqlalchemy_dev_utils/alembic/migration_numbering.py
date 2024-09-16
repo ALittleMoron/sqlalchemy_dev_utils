748a0d9d156e3ab1d424097f73e1a747ec813b84
Dmitriy Lunev <dima.lunev14@gmail.com>
2024-09-16 15:51:47 +0300

    v1.1.0: add new alembic migration name generator.

--- sqlalchemy_dev_utils/alembic/migration_numbering.py
+++ sqlalchemy_dev_utils/alembic/migration_numbering.py
@@ -25,3 +25,20 @@ def process_revision_directives_datetime_order(
     rev_id = get_utc_now().strftime("%Y%m%d%H%M%S")
     for directive in directives:
         directive.rev_id = rev_id
+
+
+def process_revision_directives_small_int_order(
+    context: "MigrationContext",  # noqa: F401
+    revision: "RevisionType",  # noqa: F401
+    directives: list["MigrationScript"],
+) -> None:
+    """``process_revision_directives`` function for alembic migration file naming.
+
+    Use in content.configure method:
+
+    https://alembic.sqlalchemy.org/en/latest/api/runtime.html#alembic.runtime.environment.EnvironmentContext.configure
+    """
+    migration_script = directives[0]
+    head_revision = context.get_current_revision()
+    new_rev_id = int(head_revision) + 1 if head_revision else 1
+    migration_script.rev_id = f"{new_rev_id:04}"
